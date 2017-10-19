8abd67604d855da6c28ebd916fb37ef01b87ea84
Bohdan Glushko <glushko.bohdan@gmail.com>
2017-10-20 00:45:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+	var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    
+    //Some click listeners will be listed here soon.
+    
+});
